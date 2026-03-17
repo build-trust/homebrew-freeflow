88c33b2cb6d4d51d8621e7cbbe34aeef4b570f4b
Mrinal Wadhwa <mrinal@ockam.io>
2026-03-17 01:03:50 -0700

    Update FreeFlow to 0.1.0-rc.3

--- Casks/freeflow.rb
+++ Casks/freeflow.rb
@@ -1,6 +1,6 @@
 cask "freeflow" do
-  version "0.1.0-rc.2"
-  sha256 "09e5e75af886e14e93fb278d3445cb569417fa6365c394404591afc005210a47"
+  version "0.1.0-rc.3"
+  sha256 "a2ed8797789b4dd72d2fb38b498227f6f2c77fe59c85032e6a6ebcb43ccef230"
 
   url "https://github.com/build-trust/freeflow/releases/download/v#{version}/FreeFlow.dmg"
   name "FreeFlow"
