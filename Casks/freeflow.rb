b273e52664ccaf8753b785574b6376945c9183b4
Mrinal Wadhwa <mrinal@ockam.io>
2026-04-18 09:47:07 -0700

    Update GitHub URLs after repo transfer to mrinalwadhwa

--- Casks/freeflow.rb
+++ Casks/freeflow.rb
@@ -2,10 +2,10 @@ cask "freeflow" do
   version "0.1.0-rc.6"
   sha256 "223b1638a23963618dc02a54964aae26606ba54670bbcf01fa2fc0a41375877c"
 
-  url "https://github.com/build-trust/freeflow/releases/download/v#{version}/FreeFlow.dmg"
+  url "https://github.com/mrinalwadhwa/freeflow/releases/download/v#{version}/FreeFlow.dmg"
   name "FreeFlow"
   desc "Press a hotkey, dictate naturally, polished text appears in any app."
-  homepage "https://github.com/build-trust/freeflow"
+  homepage "https://github.com/mrinalwadhwa/freeflow"
 
   depends_on macos: ">= :sonoma"
 
