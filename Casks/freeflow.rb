f9644858dce363560c260a860fb75ac2abc4b978
Wolfgang Meyers <wolfgang@Wolfgangs-MacBook-Pro.local>
2026-07-24 07:23:20 -0700

    Clear FreeFlow settings on uninstall

--- Casks/freeflow.rb
+++ Casks/freeflow.rb
@@ -4,16 +4,25 @@ cask "freeflow" do
 
   url "https://github.com/mrinalwadhwa/freeflow/releases/download/v#{version}/FreeFlow.dmg"
   name "FreeFlow"
-  desc "Press a hotkey, dictate naturally, polished text appears in any app."
+  desc "Press a hotkey, dictate naturally, polished text appears in any app"
   homepage "https://github.com/mrinalwadhwa/freeflow"
 
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "FreeFlow.app"
 
+  uninstall_postflight do
+    system_command "/usr/bin/defaults",
+                   args:         ["delete", "computer.autonomy.freeflow"],
+                   sudo:         false,
+                   must_succeed: false
+  end
+
   zap trash: [
-    "~/Library/Preferences/computer.autonomy.freeflow.plist",
-    "~/Library/Caches/computer.autonomy.freeflow",
     "~/Library/Application Support/FreeFlow",
+    "~/Library/Caches/computer.autonomy.freeflow",
+    "~/Library/Containers/computer.autonomy.freeflow",
+    "~/Library/Preferences/computer.autonomy.freeflow.plist",
+    "~/Library/Saved Application State/computer.autonomy.freeflow.savedState",
   ]
 end
